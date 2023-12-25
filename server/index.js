960e68712aaefdbcb3f11dc46b8b6f428a177837
Pratik Dhungel <pratikdhungel7.0@gmail.com>
2023-12-25 08:56:05 +0545

    feature: nested and related data

--- server/index.js
+++ server/index.js
@@ -24,6 +24,25 @@ const resolvers = {
     author(_, args) {
       return db.authors.find((author) => author.id === args.id);
     }
+  },
+  //Resolver for a single game object which will be called after the query
+  Game: {
+    reviews(parent) {
+      return db.reviews.filter((eachR) => eachR.game_id === parent.id);
+    }
+  },
+  Author: {
+    reviews(parent) {
+      return db.reviews.filter((eachR) => eachR.author_id === parent.id);
+    }
+  },
+  Review: {
+    game(parent) {
+      return db.games.find((eachG) => eachG.id === parent.game_id);
+    },
+    author(parent) {
+      return db.authors.find((eachA) => eachA.id === parent.author_id);
+    }
   }
 };
 
